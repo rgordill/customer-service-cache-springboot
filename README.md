82829aa4943afb17c7ab4fce66f4661f7102b02f
Katia Aresti <karesti@redhat.com>
2021-02-01 12:23:51 +0100

    proto poc

--- README.md
+++ README.md
@@ -4,4 +4,14 @@ The rationale behind this sample and the implementation details are expressed on
 
 If you find anything relevant to make it easier or you need some extra information, don't hesitate to open an PR/issue.
 
-Thanks to those that have helped me on the development, particularly those in [jkube slack channel](https://app.slack.com/client/T027F3GAJ/CKATNCPA4) and in the [infinispan zulip channel](https://infinispan.zulipchat.com/) 
+Thanks to those that have helped me on the development, particularly those in [jkube slack channel](https://app.slack.com/client/T027F3GAJ/CKATNCPA4) and in the [infinispan zulip channel](https://infinispan.zulipchat.com/)
+
+## Remote + Protostream
+
+For a quick test
+
+1. Start the infinispan server and the app in remote mode
+2. From the console, create a cache called `customers` using the "example.PROTOBUF_DIST" template
+3. Init the schema in the server with `http GET http://localhost:8080/init`
+4. Check `Data Container / Schemas tab` in the console. You should see a `customer.proto`
+5. Put a customer `http PUT http://localhost:8080/customer < data/customer-1.json`
--- src/main/java/org/sample/Customer.java
+++ src/main/java/org/sample/Customer.java
@@ -1,21 +1,23 @@
 package org.sample;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.infinispan.protostream.annotations.ProtoFactory;
+import org.infinispan.protostream.annotations.ProtoField;
 
 @SuppressWarnings("serial")
-@Getter @Setter @NoArgsConstructor 
+@Setter @NoArgsConstructor
 @Schema(title = "Customer")
 @JsonPropertyOrder({"id","first-name","last-name","email"})
-public class Customer implements Serializable {
+public class Customer {
 
 	@Schema(title="UUID of a customer", readOnly=true )
 	@JsonProperty("id")
@@ -32,5 +34,54 @@ public class Customer implements Serializable {
 	@Schema(title="Email" )
 	@JsonProperty("email")
     private String email;
-    	
+
+	@ProtoFactory
+	public Customer(String id, String firstName, String lastName, String email) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	@ProtoField(number = 1)
+	public String getId() {
+		return id;
+	}
+
+	@ProtoField(number = 2)
+	public String getLastName() {
+		return lastName;
+	}
+
+	@ProtoField(number = 3)
+	public String getFirstName() {
+		return firstName;
+	}
+
+	@ProtoField(number = 4)
+	public String getEmail() {
+		return email;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer{" + "id='" + id + '\'' + ", firstName='" + firstName + '\'' + ", lastName='" + lastName + '\''
+				+ ", email='" + email + '\'' + '}';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Customer customer = (Customer) o;
+		return Objects.equals(id, customer.id) && Objects.equals(firstName, customer.firstName) && Objects
+				.equals(lastName, customer.lastName) && Objects.equals(email, customer.email);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, firstName, lastName, email);
+	}
 }
